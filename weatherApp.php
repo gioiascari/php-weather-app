abe55347fec38acd59a9846cdb8d74b15eb21182
gioiascari <gioia.ascari96@gmail.com>
2022-07-13 21:50:04 +0200

    add some style

--- weatherApp.php
+++ weatherApp.php
@@ -9,14 +9,27 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <!-- /Bootstrap -->
     <link rel="stylesheet" href="css/style.css" />
-   
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.20.0/axios.min.js"></script>
     <title>PHP WeatherApp</title>
   </head>
 
 <body>
-  <h1>
-    ao
-  </h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-12 text-center">
+        <h4>
+            Search global weather
+        </h4>
+        <form action="" method="GET">
+          <label for="city">Enter your city</label>
+          <div>
+            <input type="text" name="text" id="text" placeholder="city">
+          </div>
+          <button type="submit" name="submit" class="btn btn-outline-warning ">Submit</button>
+        </form>
+      </div>
+    </div>
+  </div>
 </body>
 </html>
